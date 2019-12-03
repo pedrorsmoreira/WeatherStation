ffc0be045390dbd765ca8d9f757caf2656984bca
irmaoshiva <rsmoreira.pedro@gmail.com>
2019-12-03 03:19:23 +0000

    teste19

--- Ecos/structure.h
+++ Ecos/structure.h
@@ -47,6 +47,28 @@
 #define CODE_DTTL 19
 #define CODE_PR 20
 
+//PPP
+/* It is assumed that SOM and EOM values do not occur in the message */
+#define SOM 0xFD /* start of message */
+#define EOM 0xFE /* end of message */
+#define RCLK 0xC0 /* read clock */
+#define SCLK 0XC1 /* set clock */
+#define RTL 0XC2 /* read temperature and luminosity */
+#define RPAR 0XC3 /* read parameters */
+#define MMP 0XC4 /* modify monitoring period */
+#define MTA 0XC5 /* modify time alarm */
+#define RALA 0XC6 /* read alarms (temperature, luminosity, active/inactive) */
+#define DATL 0XC7 /* define alarm temperature and luminosity */
+#define AALA 0XC8 /* activate/deactivate alarms */
+#define IREG 0XC9 /* information about registers (NREG, nr, iread, iwrite)*/
+#define TRGC 0XCA /* transfer registers (curr. position)*/
+#define TRGI 0XCB /* transfer registers (index) */
+#define NMFL 0XCC /* notification memory (half) full */
+#define CMD_OK 0 /* command successful */
+#define CMD_ERROR 0xFF /* error in command */
+
+
+
 //Request message struct
 typedef struct Request {
     int cmd;
